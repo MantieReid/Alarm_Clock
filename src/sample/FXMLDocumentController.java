6f39a3ff4e3adf2a4eab6776a8795fa30e255744
mantie <mantiereid2@gmail.com>
2019-08-30 22:04:36 -0400

    Added more the event handler. Will add the rest later

--- src/sample/FXMLDocumentController.java
+++ src/sample/FXMLDocumentController.java
@@ -6,20 +6,23 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import  javafx.fxml.FXML;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.util.Duration;
 
+import java.text.*;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.Locale;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.layout.*;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -28,10 +31,20 @@ import javafx.stage.Stage;
 public class FXMLDocumentController {
 
   @FXML
+  public Label user_alarm_time_inputted;
+
+  @FXML
   Button set_alarm_button;
 
   @FXML
-  TextField user_desired_alarm_time;
+  ComboBox user_hour_combo_box;
+  @FXML
+  ComboBox user_minute_combo_box;
+
+  @FXML
+  ComboBox user_amorpm_combo_box;
+
+
 
   @FXML
   Label  label_to_show_current_time;
@@ -41,8 +54,16 @@ public class FXMLDocumentController {
 
   public  void Handle_button_click_for_set_alarm_button() {
 
-    user_desired_alarm_time.getText();
-    System.out.println(user_desired_alarm_time.getText());
+    String selected_hour = user_hour_combo_box.getValue().toString();
+    String selected_minute = user_minute_combo_box.getValue().toString();
+    System.out.println(selected_hour);
+    System.out.println("selected minute is" + selected_minute);
+    //Populate options for the comboBox.
+
+
+
+    //user_alarm_time_inputted.setText(usereneteredtime);
+
 
 
 
@@ -51,7 +72,21 @@ public class FXMLDocumentController {
 
 
   @FXML
-  public void  initialize() {
+  public void  initialize() throws ParseException {
+    String strtime = "8/30/19 3:45:22 PM";
+    DateFormat df = new SimpleDateFormat("M/dd/yy hh:mm:ss a", Locale.US);
+    Date date23;
+    date23 = df.parse(strtime);
+
+    System.out.println(df.format(date23));
+
+
+
+    user_hour_combo_box.getItems().setAll("1","2","3","4","5","6","7","8","9","10","11","12");
+    user_minute_combo_box.getItems().setAll("00","1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59");
+    user_amorpm_combo_box.getItems().setAll("AM","PM");
+
+
 
     DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("h:mm a", Locale.US);
 
@@ -59,6 +94,7 @@ public class FXMLDocumentController {
     Timeline clock = new Timeline(new KeyFrame(Duration.ZERO, e ->  {
       LocalTime timenow = LocalTime.now();
       String displaytime = timenow.format(timeFormatter);
+
       //String timenowstring = timenow.toString();
       label_to_show_current_time.setText(displaytime);
 
